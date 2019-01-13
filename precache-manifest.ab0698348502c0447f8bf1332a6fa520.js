92f1e4474b0b766912be1878137962dd8fe02384
umangg21 <umangg21@gmail.com>
2019-01-13 19:19:58 +0530

    Updates

---
+++ precache-manifest.ab0698348502c0447f8bf1332a6fa520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "ca2c765946cada1ce876",
+    "url": "/contact-manager/static/js/main.ca2c7659.chunk.js"
+  },
+  {
+    "revision": "053d9d19591a9d0b03b8",
+    "url": "/contact-manager/static/js/1.053d9d19.chunk.js"
+  },
+  {
+    "revision": "ca2c765946cada1ce876",
+    "url": "/contact-manager/static/css/main.ebadd8b9.chunk.css"
+  },
+  {
+    "revision": "0c2255541d8be536224f4c5bfef34ff2",
+    "url": "/contact-manager/index.html"
+  }
+];
